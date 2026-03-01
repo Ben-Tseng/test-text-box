21f74b0a11d36d92e74222232eff5910a89b6946
Ben-Tseng <bentseng06@gmail.com>
2026-03-01 14:41:58 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,3 @@
-https://eu-west-1.quicksight.aws.amazon.com/sn/account/amazonbi/dashboards/1c33135c-4187-40aa-98a9-26720ea3678f/sheets/1c33135c-4187-40aa-98a9-26720ea3678f_77e89b5c-5e2d-46e4-a095-e8365b0299af
+[...document.querySelectorAll('button')]
+  .find(b => b.textContent.trim() === 'Go')
+  ?.click();
