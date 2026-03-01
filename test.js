a4e3d29619568e759cbf36b0c16fac5331fd3e25
Ben-Tseng <bentseng06@gmail.com>
2026-03-01 15:09:29 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 [...document.querySelectorAll('button')]
   .find(b => b.textContent.trim() === 'Go')
   ?.click();
+
+javascript:(()=>{var b=[].slice.call(document.querySelectorAll('button')).find(function(x){return x.textContent.trim()==='Go'});if(b)b.click();})()
