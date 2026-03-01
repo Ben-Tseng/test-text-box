d7d240ec6c7121d2a16eaa33fa5951e1b701cca9
Ben-Tseng <bentseng06@gmail.com>
2026-03-01 19:06:39 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,5 @@
   .find(b => b.textContent.trim() === 'Go')
   ?.click();
 
+
+javascript:(()=>{[...document.querySelectorAll('button')].find(b=>b.textContent.trim()==='Transfer Case')?.click();})()
