cdb49c20b86950e5ecba82f50828f92988ba506d
Ben-Tseng <bentseng06@gmail.com>
2026-03-04 17:55:54 +0800

    Create testInput.js

---
+++ testInput.js
@@ -0,0 +1,13 @@
+const el = document.getElementById("BusinessRegistrationNumber");
+const value = "12345678";
+
+// 用原生 value setter，避免只改到属性不进框架
+const setter = Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, "value").set;
+setter.call(el, value);
+
+// 触发事件，让 React/表单库接收到变更
+el.dispatchEvent(new Event("input", { bubbles: true }));
+el.dispatchEvent(new Event("change", { bubbles: true }));
+
+// 有些页面还会监听 blur
+el.dispatchEvent(new Event("blur", { bubbles: true }));
