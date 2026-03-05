1f189244bcd1ef0fbb820ce0df883a2691108301
Ben-Tseng <bentseng06@gmail.com>
2026-03-05 21:11:43 +0800

    Update fill_textInBox_lite.js

--- fill_textInBox_lite.js
+++ fill_textInBox_lite.js
@@ -1,19 +1,27 @@
-(function () {
+function findInFrames(win) {
+  try {
+    const doc = win.document;
 
-  const nameEl = document.querySelector('.no-side-padding + .normal-input strong');
-  const textarea = document.querySelector('#annotationText');
+    const nameEl = doc.querySelector('.no-side-padding + .normal-input strong');
+    const ta = doc.querySelector('#annotationText');
 
-  if (!nameEl || !textarea) return;
+    if (nameEl) window.bizName = nameEl.innerText.trim();
+    if (ta) window.annotation = ta;
 
-  const value = nameEl.innerText.trim();
+    for (let i = 0; i < win.frames.length; i++) {
+      findInFrames(win.frames[i]);
+    }
+  } catch (e) {}
+}
 
+findInFrames(window);
+
+if (window.bizName && window.annotation) {
   const setter = Object.getOwnPropertyDescriptor(
     HTMLTextAreaElement.prototype,
     "value"
   ).set;
 
-  setter.call(textarea, value);
-
-  textarea.dispatchEvent(new Event("input", { bubbles: true }));
-
-})();
+  setter.call(window.annotation, window.bizName);
+  window.annotation.dispatchEvent(new Event("input", { bubbles: true }));
+}
