cdfabdd008ea86d16f2b923749bf30604c74bcc5
Ben-Tseng <bentseng06@gmail.com>
2026-09-15 13:46:56 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
    document.querySelector('a[href*="dvr-search"]').click();
+
+const bivLink = [...innerDoc.querySelectorAll('a.calypso-link')]
+  .find(a => a.textContent.trim() === 'BIV');
+
+if (bivLink) bivLink.click();
