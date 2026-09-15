1a6853990858397e78961d74395e30945171e82a
Ben-Tseng <bentseng06@gmail.com>
2026-09-15 13:39:34 +0800

    Refactor test.js to remove unused code and add link click
    Removed unused function and added link click functionality.

--- test.js
+++ test.js
@@ -1,14 +1,3 @@
-    const tabs = await getWorkflowTargetTabs(workflow);
-
-async function getWorkflowTargetTabs(workflow) {
-  const tabs = await getTargetTabs(workflow.runScope);
-  return tabs.filter((tab) => ruleMatchesTab(workflow, tab));
-}
-
-
-// powershell 
-$WShell = New-Object -ComObject Wscript.Shell
-while (1) {
-    $WShell.SendKeys("{CAPSLOCK}")
-    Start-Sleep -Seconds 300
-}
+const link = [...document.querySelectorAll('a')]
+  .find(a => a.textContent.includes('Workflow History'));
+if (link) link.click();
