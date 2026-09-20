249f0bf0605cbcf7d482859a1091891fbfc7084d
Ben-Tseng <bentseng06@gmail.com>
2026-09-20 15:32:06 +0800

    Add logic to select element based on multiple queries

--- test.js
+++ test.js
@@ -3,3 +3,11 @@ const bizLabel = [...bizDoc.querySelectorAll(".no-side-padding, div, span, label
     );
     const bizRow = bizLabel?.closest(".row") || bizLabel?.parentElement;
     const e2 = bizRow?.querySelector(".normal-input strong") || bizRow?.querySelector("strong");
+
+
+
+const element = 
+    bizDoc.querySelector('h3.sop-section-header') || 
+    bizDoc.querySelector('.sop-section-header-div') || 
+    bizDoc.querySelector('article[id*="calypso_bl"]') || 
+    e2;
